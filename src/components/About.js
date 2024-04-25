271b98c31cc26e6363846d04281469750f7bb582
princess <princessmumbi001@gmail.com>
2024-04-25 09:21:26 +0300

    hhoks jsx lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">About
+  <h2>About me</h2>
+  <img src={image} alt="I made this" />
+  <p>Hey can you imagine I actually made this</p>
+  
+  
+  </div>
+  );
 }
 
 export default About;
